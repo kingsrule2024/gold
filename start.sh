48ea5b6078c3c7775aec999e020f8fe4e6ade02a
kingsrule2024 <kingsrule5000@gmail.com>
2025-09-21 11:22:33 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,1 +0,0 @@
-./gpuminer --pubkey=3KFyMUee6eD3GhYPqQZ2bnax7ByDwpffEjPmkKBWkaK3WQU2joghU1nQBrjEVRkJvGo4BFR1PgtMpSiuFSr4FwqwMZQbNePkHVwFu7R5PCyMz5LfrceGF6gBQ6Wvdfby8A3P --name=$(hostname) --label=Rental
